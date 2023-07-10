c5ba2c0c87acf615fa5ea4a78fb89f1ba95c4566
unknown <brennerpaiva@gmail.com>
2023-07-10 14:02:59 -0300

    feat: create input component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Input from "./component/Input";
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Input />
     </div>
   );
 }
---
+++ src/component/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Input() {
+  return (
+    <div>
+      Input - todo app
+      <input type="text" />
+      <button>Add Task</button>
+    </div>
+  );
+}
+
+export default Input;
