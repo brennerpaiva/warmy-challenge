140e4e62ade8f1f4ce5558bb33731dcd9240ab44
unknown <brennerpaiva@gmail.com>
2023-07-10 22:25:43 -0300

    feat: using context-api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Input from "./component/Input";
+import Input from "./components/Input";
+import TaskItem from "./components/TaskItem";
+import { TodoContext } from "./context/TodoProvider";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container w-full mx-auto max-w-xl p-2 font-poppins">
+      <h1 className="text-center bold text-4xl">To Do</h1>
       <Input />
+      <TodoContext.Consumer>
+        {(state) =>
+          state.todoList.map((todo) => <TaskItem key={todo.id} todo={todo} />)
+        }
+      </TodoContext.Consumer>
     </div>
   );
 }
