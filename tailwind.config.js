6d0e9aaf5a20d212d5dd4d2295c4a1d8b86b797e
unknown <brennerpaiva@gmail.com>
2023-07-10 22:18:37 -0300

    chore: new config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#72CACA",
+        secondary: "#BFBFBF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
